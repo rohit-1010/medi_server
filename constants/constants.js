236c06f4994ed4763da31d34b563a2b8793e10f6
rohit-1010 <rohitcryp77@gmail.com>
2025-11-13 21:50:58 +0530

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
 export const PORT = 5000;
 export const MONGO_URI = "mongodb+srv://aarthi_db_user:lcMEAXOKBKgCxYnI@cluster.hbb0ymu.mongodb.net/?appName=Cluster";
 export const JWT_SECRET = "BWiWl5AKiPVU58uLfqzNlT/TJpP23oC4Z+38K8C6AW5cVUw/ZSuHS6cQBxDI/LvGsfVMAcWas0fe9h3Fan8gPw==";
-export const EMAIL_USER = "aarthiraju@datapro.in";
-export const EMAIL_PASS = "rrep gqff evcz fzdv";
+export const EMAIL_USER = "koirirohitps@gmail.com";
+export const EMAIL_PASS = "ukjd yqmd ctgs tsnk";
