f910ef5de084d000aa45cb003519e96dacf41538
rohit-1010 <rohitcryp77@gmail.com>
2025-11-14 10:26:23 +0530

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,17 @@
-export const PORT = 5000;
-export const MONGO_URI = "mongodb+srv://aarthi_db_user:lcMEAXOKBKgCxYnI@cluster.hbb0ymu.mongodb.net/?appName=Cluster";
-export const JWT_SECRET = "BWiWl5AKiPVU58uLfqzNlT/TJpP23oC4Z+38K8C6AW5cVUw/ZSuHS6cQBxDI/LvGsfVMAcWas0fe9h3Fan8gPw==";
-export const EMAIL_USER = "koirirohitps@gmail.com";
-export const EMAIL_PASS = "ukjd yqmd ctgs tsnk";
+// export const PORT = 5000;
+// export const MONGO_URI = "mongodb+srv://aarthi_db_user:lcMEAXOKBKgCxYnI@cluster.hbb0ymu.mongodb.net/?appName=Cluster";
+// export const JWT_SECRET = "BWiWl5AKiPVU58uLfqzNlT/TJpP23oC4Z+38K8C6AW5cVUw/ZSuHS6cQBxDI/LvGsfVMAcWas0fe9h3Fan8gPw==";
+// export const EMAIL_USER = "aarthiraju@datapro.in";
+// export const EMAIL_PASS = "rrep gqff evcz fzdv";
+
+
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
+export const PORT = process.env.PORT;
+export const MONGO_URI = process.env.MONGO_URI;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const EMAIL_USER = process.env.EMAIL_USER;
+export const EMAIL_PASS = process.env.EMAIL_PASS;
